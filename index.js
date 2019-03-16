8f10d2096cbea88c2e64b3d461bab3bb4ae86ea5
Kalle Lämsä <1397804+coocos@users.noreply.github.com>
2019-03-16 14:00:42 +0200

    Scale by aspect ratio when generating camera rays

--- index.js
+++ index.js
@@ -1,23 +1,26 @@
 import { Vector } from "vectorious";
 import { Ray, Sphere } from "./primitives";
 
-const camera = new Vector([0, 0, 2]);
-const screen = {
-  topLeft: new Vector([-1, 1, 0]),
-  bottomRight: new Vector([1, -1, 0])
-};
-const uv = Vector.subtract(screen.bottomRight, screen.topLeft);
-
 const canvas = document.getElementById("canvas");
 const context = canvas.getContext("2d");
+const camera = new Vector([0, 0, 2]);
 const sphere = new Sphere(new Vector([0, 0, -2]), 1);
 const sphereColor = new Vector(new Uint8Array([255, 255, 255]));
 const light = new Vector([0, 0, 2]);
 
-for (let y = 0; y < 256; y++) {
-  for (let x = 0; x < 256; x++) {
-    const u = uv.x * x / 256;
-    const v = uv.y * y / 256;
+const screenWidth = canvas.width;
+const screenHeight = canvas.height;
+const aspectRatio = screenWidth / screenHeight;
+const screen = {
+  topLeft: new Vector([-1 * aspectRatio, 1, 0]),
+  bottomRight: new Vector([1 * aspectRatio, -1, 0])
+};
+const uv = Vector.subtract(screen.bottomRight, screen.topLeft);
+
+for (let y = 0; y < screenHeight; y++) {
+  for (let x = 0; x < screenWidth; x++) {
+    const u = uv.x * x / screenWidth;
+    const v = uv.y * y / screenHeight;
     const direction = Vector.add(screen.topLeft, new Vector([u, v, 0]))
       .subtract(camera)
       .normalize();
@@ -27,8 +30,8 @@ for (let y = 0; y < 256; y++) {
     if (point !== undefined) {
       const normal = sphere.normal(point);
       const surfaceToLight = Vector.subtract(light, point).normalize();
-      const diffuse = Math.max(0, normal.dot(surfaceToLight));
-      const color = Vector.scale(sphereColor, diffuse);
+      const lambertian = Math.max(0, normal.dot(surfaceToLight));
+      const color = Vector.scale(sphereColor, lambertian);
       context.fillStyle = `rgb(${color.x}, ${color.y}, ${color.z})`;
       context.fillRect(x, y, 1, 1);
     } else {
