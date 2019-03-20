b6d306c0465e0633127c12692f6636ed0f6e121f
Kalle Lämsä <1397804+coocos@users.noreply.github.com>
2019-03-20 18:28:21 +0200

    Implement single bounce reflections

--- index.js
+++ index.js
@@ -51,6 +51,20 @@ function trace(ray, breakEarly = false) {
   return [point, primitive];
 }
 
+function shade(point, normal, primitive, ray) {
+  //Calculate Lambertian reflectance / diffuse
+  const toLight = Vector.subtract(light, point).normalize();
+  const lambertian = Math.max(0, normal.dot(toLight));
+  const color = Vector.scale(primitive.color, lambertian);
+
+  //Calculate specular reflection using Blinn-Phong
+  const toCamera = Vector.scale(ray.direction, -1);
+  const halfVector = Vector.add(toLight, toCamera).scale(0.5);
+  const specular = Math.max(0, halfVector.dot(normal)) ** 16;
+  color.add(Vector.scale(white, specular));
+  return color;
+}
+
 for (let y = 0; y < screenHeight; y++) {
   for (let x = 0; x < screenWidth; x++) {
     //Construct ray from camera to pixel plane
@@ -66,19 +80,24 @@ for (let y = 0; y < screenHeight; y++) {
     //Shade intersected primitive
     if (primitive !== null) {
       const normal = primitive.normal(point);
+      const color = shade(point, normal, primitive, ray);
 
-      //Calculate Lambertian reflectance / diffuse
-      const toLight = Vector.subtract(light, point).normalize();
-      const lambertian = Math.max(0, normal.dot(toLight));
-      const color = Vector.scale(primitive.color, lambertian);
-
-      //Calculate specular reflection using Blinn-Phong
-      const toCamera = Vector.scale(ray.direction, -1);
-      const halfVector = Vector.add(toLight, toCamera).scale(0.5);
-      const specular = Math.max(0, halfVector.dot(normal)) ** 16;
-      color.add(Vector.scale(white, specular));
+      //Calculate if point reflects another object in the scene
+      const reflectedRay = new Ray(point, ray.reflect(normal));
+      const [reflectedPoint, reflectedPrimitive] = trace(reflectedRay, true);
+      if (reflectedPoint !== null) {
+        const reflectedNormal = reflectedPrimitive.normal(reflectedPoint);
+        const reflectedColor = shade(
+          reflectedPoint,
+          reflectedNormal,
+          reflectedPrimitive,
+          reflectedRay
+        );
+        color.scale(0.5).add(reflectedColor.scale(0.5));
+      }
 
       //Check if point is shadowed
+      const toLight = Vector.subtract(light, point).normalize();
       const shadowRay = new Ray(point, toLight);
       const [shadowedPoint, shadowedPrimitive] = trace(shadowRay, true);
       if (shadowedPoint !== null) {
