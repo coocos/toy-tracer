4bedb434b0bcaf691a7a02ed87547827efb36194
Kalle Lämsä <1397804+coocos@users.noreply.github.com>
2019-04-01 21:46:46 +0300

    Use multiple WebWorkers for parallel computation

--- index.js
+++ index.js
@@ -16,32 +16,45 @@ const screen = {
   bottomRight: new Vector([1 * aspectRatio, -1, 0])
 };
 
-// Post render tasks to WebWorkers
-const worker = new Worker("worker.js");
-worker.onmessage = ({ data }) => {
-  for (const [x, ys] of Object.entries(data.bucket)) {
-    for (const [y, color] of Object.entries(ys)) {
-      context.fillStyle = color;
-      context.fillRect(x, y, 1, 1);
-    }
+/**
+ * Spawns multiple WebWorkers for rendering the scene.  Each worker will
+ * compute pixel colors for a single vertical slice of the canvas.
+ *
+ * @param {Number} workerCount - amount of workers to spawn
+ */
+function spawnWorkers(workerCount = 1) {
+  const verticalSlice = Math.floor(screenHeight / workerCount);
+  for (let i = 0; i < workerCount; i++) {
+    // Post render tasks to WebWorkers
+    const worker = new Worker("worker.js");
+    worker.onmessage = ({ data }) => {
+      for (const [x, ys] of Object.entries(data.bucket)) {
+        for (const [y, color] of Object.entries(ys)) {
+          context.fillStyle = color;
+          context.fillRect(x, y, 1, 1);
+        }
+      }
+      console.log(`Rendering took ${new Date() - renderStartTime} ms`);
+    };
+    worker.postMessage({
+      screen: {
+        topLeft: screen.topLeft.toArray(),
+        bottomRight: screen.bottomRight.toArray()
+      },
+      resolution: {
+        width: canvas.width,
+        height: canvas.height
+      }
+    });
+    worker.postMessage({ scene: serialize(scene) });
+    const renderStartTime = new Date();
+    worker.postMessage({
+      bucket: {
+        x: [0, screenWidth],
+        y: [i * verticalSlice, i * verticalSlice + verticalSlice]
+      }
+    });
   }
-  console.log(`Rendering took ${new Date() - renderStartTime} ms`);
-};
-worker.postMessage({
-  screen: {
-    topLeft: screen.topLeft.toArray(),
-    bottomRight: screen.bottomRight.toArray()
-  },
-  resolution: {
-    width: canvas.width,
-    height: canvas.height
-  }
-});
-worker.postMessage({ scene: serialize(scene) });
-const renderStartTime = new Date();
-worker.postMessage({
-  bucket: {
-    x: [0, screenWidth],
-    y: [0, screenHeight]
-  }
-});
+}
+
+spawnWorkers(2);
