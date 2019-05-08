c2a233720149fe7e4ad3d2359f5fd3a97179a5ff
Kalle Lämsä <1397804+coocos@users.noreply.github.com>
2019-05-08 19:04:24 +0300

    Test Sphere class

---
+++ sphere.spec.js
@@ -0,0 +1,70 @@
+import { expect } from "chai";
+import { Ray, Vector } from "./math";
+import { Sphere } from "./primitives";
+
+describe("Sphere", () => {
+  it("should map constructor arguments to attributes", () => {
+    const position = new Vector(0, 0, 0);
+    const color = new Vector(255, 0, 0);
+    const radius = 5;
+
+    const sphere = new Sphere(position, radius, { color });
+    expect(sphere.position).to.equal(position);
+    expect(sphere.radius).to.equal(radius);
+    expect(sphere.material.color).to.equal(color);
+  });
+  it("should return correct normal", () => {
+    const position = new Vector(0, 0, 0);
+    const color = new Vector(255, 0, 0);
+    const radius = 5;
+    const sphere = new Sphere(position, radius, { color });
+
+    const topPoint = new Vector(0, 5, 0);
+    expect(sphere.normal(topPoint).toArray()).to.deep.equal([0, 1, 0]);
+    const bottomPoint = new Vector(0, -5, 0);
+    expect(sphere.normal(bottomPoint).toArray()).to.deep.equal([0, -1, 0]);
+    const sidePoint = new Vector(5, 0, 0);
+    expect(sphere.normal(sidePoint).toArray()).to.deep.equal([1, 0, 0]);
+  });
+  it("should serialize to JSON", () => {
+    const position = new Vector(0, 0, 0);
+    const color = new Vector(255, 0, 0);
+    const radius = 5;
+    const sphere = new Sphere(position, radius, { color });
+    expect(sphere.serialize()).to.deep.equal({
+      type: "Sphere",
+      position: position.toArray(),
+      radius: radius,
+      material: {
+        color: color.toArray(),
+        glossiness: 0,
+        reflectivity: 0
+      }
+    });
+  });
+  it("should deserialize to an object", () => {
+    const position = new Vector(1, 2, 3);
+    const radius = 10;
+    const color = new Vector(125, 255, 125);
+    const glossiness = 16;
+    const reflectivity = 0.9;
+    const sphere = Sphere.deserialize(position.toArray(), radius, {
+      color: color.toArray(),
+      glossiness,
+      reflectivity
+    });
+    expect(sphere.position.toArray()).to.deep.equal(position.toArray());
+    expect(sphere.radius).to.deep.equal(radius);
+    expect(sphere.material.color.toArray()).to.deep.equal(color.toArray());
+    expect(sphere.material.glossiness).to.equal(glossiness);
+    expect(sphere.material.reflectivity).to.equal(reflectivity);
+  });
+  it("should return intersection between ray and itself", () => {
+    const position = new Vector(0, 0, 0);
+    const color = new Vector(255, 0, 0);
+    const radius = 1;
+    const sphere = new Sphere(position, radius, { color });
+    const ray = new Ray(new Vector(0, 0, -5), new Vector(0, 0, 1));
+    expect(sphere.intersect(ray).toArray()).to.deep.equal([0, 0, -1]);
+  });
+});
